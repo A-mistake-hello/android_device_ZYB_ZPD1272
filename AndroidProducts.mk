4a8e61e6d3695465dfa7dc538608fdccf117d104
XingKongTanSuo <3329989998@qq.com>
2025-09-14 09:59:32 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -9,7 +9,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/omni_tb8786p1_64_k510_wifi.mk
 
 COMMON_LUNCH_CHOICES := \
-    omni_tb8786p1_64_k510_wifi-user \
-    omni_tb8786p1_64_k510_wifi-userdebug \
-    omni_tb8786p1_64_k510_wifi-eng
+    twrp_tb8786p1_64_k510_wifi-user \
+    twrp_tb8786p1_64_k510_wifi-userdebug \
+    twrp_tb8786p1_64_k510_wifi-eng
     
